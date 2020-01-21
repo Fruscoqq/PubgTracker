e7877900cf8ca9b4b3d5c377efae13479a426868
Fruscoqq <senter64@gmail.com>
2020-01-21 23:51:06 +0000

    removed User from navbar

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar sticky-top navbar-expand-md navbar-light text-uppercase navbar-dark mr-auto">
-       <Link className="navbar-brand" to="/"><img src={navLogo} alt="Nav Bar Logo" className="d-line-block align-top" style={{ width: '120px', height: '120px' }} /></Link>
+        <Link className="navbar-brand" to="/"><img src={navLogo} alt="Nav Bar Logo" className="d-line-block align-top" style={{ width: '120px', height: '120px' }} /></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -14,7 +14,6 @@ const Navbar = () => {
           <div className="navbar-nav">
             <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
             <Link className="nav-item nav-link" to="/Player/Leaderboard">Leaderboard</Link>
-            <Link className="nav-item nav-link" to="/Player/Stats">User</Link>
           </div>
         </div>
       </nav>
