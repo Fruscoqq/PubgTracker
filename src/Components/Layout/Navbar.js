33f081873bd5c6ab53feb7f92ae140d15ce793df
Fruscoqq <senter64@gmail.com>
2020-01-21 23:46:03 +0000

    trying to push to heroku

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -5,8 +5,8 @@ import navLogo from './../../images/navLogo.png';
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-md navbar-light text-uppercase navbar-dark justify-content-center">
-        <div><Link className="navbar-brand" to="/"><img src={navLogo} alt="Nav Bar Logo" className="d-line-block align-top" style={{width: '120px', height:'120px'}}/></Link></div>
+      <nav className="navbar sticky-top navbar-expand-md navbar-light text-uppercase navbar-dark mr-auto">
+       <Link className="navbar-brand" to="/"><img src={navLogo} alt="Nav Bar Logo" className="d-line-block align-top" style={{ width: '120px', height: '120px' }} /></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
